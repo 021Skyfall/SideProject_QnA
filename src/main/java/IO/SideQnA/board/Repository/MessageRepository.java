7507738893bbe60abc930b91650cbb93efbee5c1
021Skyfall <021skyfall@gmail.com>
2023-03-11 10:29:33 +0900

    v1

--- src/main/java/IO/SideQnA/board/Repository/MessageRepository.java
+++
@@ -1,7 +0,0 @@
-package IO.SideQnA.board.Repository;
-
-import IO.SideQnA.board.entity.Message;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MessageRepository extends JpaRepository<Message, Long> {
-}
--- src/main/java/IO/SideQnA/order/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package IO.SideQnA.order.repository;
-
-import IO.SideQnA.order.entity.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
